dda2a4e9c3c36c5ab76c33721828615e2f3f6635
rayyu999 <411205547@qq.com>
2020-12-14 14:34:41 +0800

    add blog editor and pic upload

--- src/main/java/cn/yuyingwai/springbootblog/config/MyBlogWebMvcConfigurer.java
+++ src/main/java/cn/yuyingwai/springbootblog/config/MyBlogWebMvcConfigurer.java
@@ -4,6 +4,7 @@ import cn.yuyingwai.springbootblog.interceptor.AdminLoginInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -19,6 +20,10 @@ public class MyBlogWebMvcConfigurer implements WebMvcConfigurer {
                 .excludePathPatterns("/admin/login")
                 .excludePathPatterns("/admin/dist/**")
                 .excludePathPatterns("/admin/plugins/**");
+    }
+
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/upload/**").addResourceLocations("file:D:\\upload\\");
     }
 
 }
