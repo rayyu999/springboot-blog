df123ca48de5fab0a3fa690578a6ee56a477cd61
rayyu999 <411205547@qq.com>
2020-12-14 11:46:37 +0800

    add editor

---
+++ src/main/java/cn/yuyingwai/springbootblog/controller/admin/BlogController.java
@@ -0,0 +1,19 @@
+package cn.yuyingwai.springbootblog.controller.admin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+@RequestMapping("/admin")
+public class BlogController {
+
+    @GetMapping("/blogs/edit")
+    public String edit(HttpServletRequest request) {
+        request.setAttribute("path", "edit");
+        return "admin/edit";
+    }
+
+}
